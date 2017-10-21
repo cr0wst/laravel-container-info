b8d139907f6f14ef86505b7ef1a59f54a999a89e
Sara Bine <sara@sarabine.com>
2017-10-21 14:26:38 -0600

    Enhance `providers:list` command
    - Renamed getRegisteredProviders to getProviderList for consistency
    - Added 'Deferred' and 'Provides' columns to provider output
    - Added --include-illuminate flag support

--- src/ProviderInformation/Services/ProviderInformation.php
+++ src/ProviderInformation/Services/ProviderInformation.php
@@ -25,15 +25,35 @@ class ProviderInformation
     /**
      * Get the list of registered providers from the Container.
      *
+     * @param bool $includeIlluminate Whether or not to include the Illuminate bindings.
+     *
      * @return array containing the registered providers.
      */
-    public function getRegisteredProviders()
+    public function getProviderList($includeIlluminate = true)
     {
         // Use reflection to get the provider array off of the Application
         // The property we're after is 'serviceProviders' on the Application class.
-        $providers = (new ReflectionClass($this->application))->getProperty('serviceProviders');
-        $providers->setAccessible(true);
+        $serviceProviderProperty = (new ReflectionClass($this->application))->getProperty('serviceProviders');
+        $serviceProviderProperty->setAccessible(true);
 
-        return $providers->getValue($this->application);
+        $providers = $serviceProviderProperty->getValue($this->application);
+
+        $foundProviders = [];
+
+        /** @var \Illuminate\Support\ServiceProvider $provider */
+        foreach ($providers as $provider) {
+            $providerClass = get_class($provider);
+
+            // Only include Illuminate bindings if they were specifically requested.
+            if ($includeIlluminate || strpos($providerClass, 'Illuminate\\') === false) {
+                $foundProviders[] = [
+                    'class'    => $providerClass,
+                    'deferred' => $provider->isDeferred() ? 'true' : '',
+                    'provides' => implode("\n", $provider->provides()),
+                ];
+            }
+        }
+
+        return $foundProviders;
     }
 }
